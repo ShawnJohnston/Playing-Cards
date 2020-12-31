86fcba602f2b9bde72bbdc893ea9b791efce8414
ShawnJohnston <44795446+ShawnJohnston@users.noreply.github.com>
2020-12-30 20:18:49 -0500

    12/30/2020

--- src/HandEvaluator.java
+++ src/HandEvaluator.java
@@ -6,6 +6,16 @@ public class HandEvaluator {
     private static String[] standardPokerRanks = {"High Card", "Pair", "Two Pair", "Trips", "Straight", "Flush",
                                                     "Full House", "Quads", "Straight Flush", "Royal Flush"};
     private String handRank;
+    private Boolean isHighCard = false;
+    private Boolean isPair = false;
+    private Boolean isTwoPair = false;
+    private Boolean isTrips = false;
+    private Boolean isStraight = false;
+    private Boolean isFlush = false;
+    private Boolean isFullHouse = false;
+    private Boolean isQuads = false;
+    private Boolean isStraightFlush = false;
+    private Boolean isRoyalFlush = false;
 
     // Constructors
     public HandEvaluator() {
@@ -22,6 +32,39 @@ public class HandEvaluator {
         HandEvaluator.gameMode = gameMode;
     }
     // Methods
+    public void checkForMultiples(ArrayList<PlayingCard> hand) {
+        String[] values = {"Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"};
+        int[] valueCounts = new int[values.length];
+        ArrayList<String> pairsList = new ArrayList<>();
+        ArrayList<String> tripsList = new ArrayList<>();
+        String quadsValue;
+        int handCount = 0;
+
+        for (int i = 0; i < values.length; i++) {
+            for (PlayingCard playingCard : hand) {
+                if (playingCard.getValue().equals(values[i])) {
+                    valueCounts[i] += 1;
+                }
+            }
+        }
+        for (int i = 0; i < values.length; i++) {
+            if (handCount >= hand.size()) {
+                break;
+            }
+            if (valueCounts[i] == 2) {
+                pairsList.add(values[i]);
+                handCount += 2;
+            }
+            if (valueCounts[i] == 3) {
+                tripsList.add(values[i]);
+                handCount += 3;
+            }
+            if (valueCounts[i] == 4) {
+                quadsValue = values[i];
+                break;
+            }
+        }
+    }
     public Boolean isAFlush(ArrayList<PlayingCard> hand) {
         String[] suits = {"Spades", "Hearts", "Clubs", "Diamonds"};
         int spadesCounter = 0, heartsCounter = 0, clubsCounter = 0, diamondsCounter = 0;
@@ -54,7 +97,7 @@ public class HandEvaluator {
         }
         return false;
     }
-    public String isAStraight(ArrayList<PlayingCard> hand) {
+    public Boolean isAStraight(ArrayList<PlayingCard> hand) {
         int straightCondition = hand.size();
         ArrayList<PlayingCard> sortedHand = new ArrayList<>();
         String[] values = {"Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"};
@@ -67,10 +110,7 @@ public class HandEvaluator {
                 }
             }
         }
-        if (straightCondition == countToStraight(sortedHand, values)) {
-            return "Straight";
-        }
-        return null;
+        return straightCondition == countToStraight(sortedHand, values);
     }
     public int countToStraight(ArrayList<PlayingCard> sortedHand, String[] values) {
         int straightCount = 1;
